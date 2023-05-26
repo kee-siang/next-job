bf1aa9756f39b5ce9bdfefb3783f8e7bcea6c4eb
kee-siang <tksiang.ss@example.com>
2023-05-26 11:26:13 +0800

    add new files for solving job id problem

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,69 @@
-<h1>Welcome to SvelteKit</h1>
+<!-- <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<a href = "/about">About my site</a> -->
+
+<!-- Testing
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+
+  <button class="btn">Button</button> -->
+<!-- 
+<button class="btn">Button</button>
+<span class="badge">Badge</span> -->
+
+
+  <!-- // import { onMount } from "svelte";
+
+  // // define a function that makes a http request to our backend to get data
+  // async function getJobs() {
+  //   const resp = await fetch("https://next-jobs-db-backend.fly.dev" + '/api/collections/jobs/records');
+
+  //   const res = await resp.json();
+  //   if (resp.status == 200) {
+  //     return {
+  //       jobs: res.items
+  //     }
+  //   } else {
+  //     return {
+  //       jobs: []
+  //     }
+  //   }
+  // }
+
+  // // initialize a data variable when our route first gets loaded
+  // let data = {
+  //   jobs: []
+  // }
+  
+  // onMount is a Svelte function that gets called as soon as our component gets loaded, we then call our getJobs function to retrieve data
+  // onMount(async () => {
+  //   data = await getJobs()
+  // }) -->
+
+  <script>
+    export let data;
+
+    import humanize from 'humanize-plus';
+  </script>
+
+
+
+<h1 class="text-center text-xl font-bold">Find Your Next Job</h1>
+
+<div class="overflow-x-auto w-full">
+  {#each data.jobs as job}
+    <div class="flex flex-col mt-10">
+      <div>
+        <a class="font-bold text-2xl" href="/jobs/{job.id}">{job.title}</a>
+        <div class="text-sm mt-1">
+          {job.employer} . {job.location} . <span class="text-sm">USD {humanize.formatNumber(job.minAnnualCompensation)} - USD {humanize.formatNumber(job.maxAnnualCompensation)}</span>
+        </div>
+        <div class="italic text-xs opacity-50 mt-2">posted {new Date(job.created).toLocaleDateString(undefined, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'})}</div>
+      </div>
+      <div class="mt-4 ">
+        {job.description.slice(0, 240)}...
+      </div>
+    </div>
+  {/each}
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+
+    // daisyUI config (optional)
+    daisyui: {
+      styled: true,
+      themes: ["cyberpunk", "dracula"],
+      base: true,
+      utils: true,
+      logs: true,
+      rtl: false,
+      prefix: "",
+      darkTheme: "dracula",
+    },
+}
+
