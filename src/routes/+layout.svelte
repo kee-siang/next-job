7e60366ce0c8c4570938f4b42726c5e389124683
kee-siang <tksiang.ss@example.com>
2023-05-31 14:40:37 +0800

    create Next Job (Home Button), sign in button and post job button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script>
-  import "../app.css";
+	import '../app.css';
 </script>
+
+<div class="flex justify-between">
+  <div>
+  <a class="text-2xl font-bold m-4 btn bg-white border-none btn-primary hover:bg-emerald-400" href="/">NEXT JOB DB</a>
+  </div>
+  <div>
+  <a href="/jobs/new"><button class="p-4 m-3 btn btn-primary bg-white border-none hover:bg-emerald-400">Post a Job</button></a>
+  <button class="p-4  m-3 btn btn-primary bg-white border-none hover:bg-emerald-400"><a href="/login">Sign In</a></button>
+  </div>
+</div>
 
 <slot />
