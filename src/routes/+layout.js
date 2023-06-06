df8902bb3e00197cef87db734854be551646a79a
kee-siang <tksiang.ss@example.com>
2023-06-06 16:50:10 +0800

    add some comments

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,1 +1,9 @@
+// Turn the whole website become SPA mode, used for frontend apps that are rely on API.//
 export const ssr = false;
+
+import { isLoggedIn } from '../utils/auth.js';
+
+//To make sure that the sign out button able to stay show out after the page is refresh// 
+export async function load() {
+    await isLoggedIn();
+}
