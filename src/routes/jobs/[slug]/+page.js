d47759eaf3ff8e9c85137d7d6e97ff4fab0fe830
kee-siang <tksiang.ss@example.com>
2023-06-06 16:35:49 +0800

    add some comments

--- src/routes/jobs/[slug]/+page.js
+++ src/routes/jobs/[slug]/+page.js
@@ -1,5 +1,6 @@
 import { PUBLIC_BACKEND_BASE_URL } from '$env/static/public';
 
+//Get data for specific job id from database using `api/collections/jobs/records/${params.slug}`//
 export async function load({ fetch, params }) {
     const resp = await fetch( PUBLIC_BACKEND_BASE_URL + `api/collections/jobs/records/${params.slug}`);
 
