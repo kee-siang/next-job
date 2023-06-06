eb9e90212df43ec92f9dab435151bb24737f83fa
kee-siang <tksiang.ss@example.com>
2023-06-06 16:47:37 +0800

    add some comments

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,8 +1,9 @@
 import { PUBLIC_BACKEND_BASE_URL } from '$env/static/public';
 
+//use to fetch data from the database through 'api/collections/jobs/records'//
 export async function load({ fetch }) {
-    const resp = await fetch(PUBLIC_BACKEND_BASE_URL + '/api/collections/jobs/records');
-  
+    const resp = await fetch(PUBLIC_BACKEND_BASE_URL + 'api/collections/jobs/records');
+    
     const res = await resp.json();
     if (resp.status == 200) {
       return {
