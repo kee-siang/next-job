8a3077540e2bd7f53a7ac192df34ff33d60a1b2a
kee-siang <tksiang.ss@example.com>
2023-06-13 23:49:46 +0800

    beautify the job details body

--- src/routes/jobs/[slug]/+page.svelte
+++ src/routes/jobs/[slug]/+page.svelte
@@ -11,50 +11,38 @@
   <title>Job Details | Next Jobs</title>
 </svelte:head>
 
-<div>
-    <div class="flex">
-        <div class="flex-1">
-            <h1 class="text-3xl font-bold underline decoration-sky-500 py-4 mb-8">Job Details</h1>
-            <div class="flex justify-between">
+<div class="mt-5 p-4">
+    <div class="flex-1">
+            <div class="flex justify-between mb-1">
             <h1 class="text-3xl font-extrabold">{data.job.title}</h1>
             <!-- Show edit button for user that has log in -->
             {#if $isAuthenticated == true && data.job.user == getUserId()}
                 <button class="text-center hover:bg-black hover:text-white btn btn-primary bg-white text-black border-none w-32"><a href="{data.job.id}/update">EDIT</a></button>
             {/if}
             </div>
-            <p class="text-xl py-4 underline decoration-pink-400">{data.job.employer}</p>
-            <span class="rounded-lg bg-red-300 border-none m-4 p-1">{data.job.location}</span>
-            <span class="rounded-lg bg-cyan-500 border-none m-4 p-1">USD {humanize.formatNumber(data.job.minAnnualCompensation)} - USD
-                {humanize.formatNumber( data.job.maxAnnualCompensation )}</span>
-        </div>
+            <p class="text-xl">{data.job.employer}</p>
     </div>
-</div>
 
-
-
-<div class="flex flex-row w-full mt-8">
-    <div class="basis-2/3 prose  max-w-none w-full">
-        <h2 class="text-xl font-thin underline decoration-amber-400">Description</h2>
-        <div>
+    <div class="flex flex-row w-full mt-8">
+        <div class="basis-2/3 prose max-w-none w-full">
+            <h2 class="text-xl font-semibold">Description</h2>
             <SvelteMarkdown source={data.job.description} />
-        </div>
-        <div class="mt-6" />
-        <h2 class="text-xl font-thin underline decoration-amber-400">Requirements</h2>
-        <div>
+            <div class="mt-6" />
+            <h2 class="text-xl font-semibold">Requirements</h2>
             <SvelteMarkdown source={data.job.requirements} />
-            <div/>
-            <div class = "mt-6" />
-            <h2 class="text-xl font-thin underline decoration-amber-400">How to Apply?</h2>
+            <div class="mt-6" />
+            <h2 class="text-xl font-semibold">How to Apply?</h2>
             <p>{data.job.applicationInstructions}</p>
         </div>
         <div class="basis-1/3 ml-4">
-            <h2 class="text-xl font-thin underline decoration-amber-400">Location</h2>
+            <h2 class="text-xl font-semibold">Location</h2>
             <p>{data.job.location}</p>
             <div class="mt-6" />
-            <h2 class="text-xl font-thin underline decoration-amber-400">Salary Range</h2>
+            <h2 class="text-xl font-semibold">Salary Range</h2>
             <p>
-                USD {humanize.formatNumber(data.job.minAnnualCompensation)} - USD
-                {humanize.formatNumber( data.job.maxAnnualCompensation )}
+                USD {humanize.formatNumber(data.job.minAnnualCompensation)} - USD {humanize.formatNumber(
+                    data.job.maxAnnualCompensation
+                )}
             </p>
         </div>
     </div>
